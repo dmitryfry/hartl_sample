783943c55532e3f5a28a50fe850a3ab9e956577a
Dmitry Fry <im@dmitryfry.com>
2018-02-15 13:42:22 +0300

    routing specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,18 +1,19 @@
 require "rails_helper"
 
-RSpec.feature "Static pages", :type => :feature do
+RSpec.describe StaticPagesController, :type => :feature do
+# RSpec.feature "Static pages", :type => :feature do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit static_pages_home_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit static_pages_home_path
       expect(page).to have_title("#{base_title} | Home")
     end
   end
@@ -20,12 +21,12 @@ RSpec.feature "Static pages", :type => :feature do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit static_pages_help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit static_pages_help_path
       expect(page).to have_title("#{base_title} | Help")
     end
   end
@@ -33,12 +34,12 @@ RSpec.feature "Static pages", :type => :feature do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit static_pages_about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit static_pages_about_path
       expect(page).to have_title("#{base_title} | About Us")
     end
   end
@@ -46,12 +47,12 @@ RSpec.feature "Static pages", :type => :feature do
   describe "Contact page" do
 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit static_pages_contact_path
       expect(page).to have_content('Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit static_pages_contact_path
       expect(page).to have_title("#{base_title} | Contact")
     end
   end
